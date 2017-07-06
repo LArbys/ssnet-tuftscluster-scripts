8a535e904f700e8fe812037d75f2d93fefa567fb
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-07-06 16:15:45 -0400

    more transparent parameters for submission script

--- submit.sh
+++ submit.sh
@@ -7,5 +7,16 @@
 #SBATCH --time=8:00:00
 #SBATCH --mem-per-cpu=4000
 
+CONTAINER=/cluster/kappa/90-days-archive/wongjiradlab/larbys/images/singularity-dllee-ubuntu/singularity-dllee-ssnet.img
+WORKDIR=/cluster/home/twongj01/grid_jobs/ssnet-tuftscluster-scripts
+INPUTLISTDIR=${WORKDIR}/inputlists
+OUTPUTDIR=/cluster/kappa/90-days-archive/wongjiradlab/larbys/data/mcc8.1/numu_1muNpfiltered/out_week0626/ssnet
+JOBIDLIST=/cluster/home/twongj01/grid_jobs/ssnet-tuftscluster-scripts/rerunlist.txt
+
+
 module load singularity
-srun singularity exec /cluster/kappa/90-days-archive/wongjiradlab/larbys/images/singularity-dllee-ubuntu/singularity-dllee-ssnet.img bash -c "cd /cluster/home/twongj01/grid_jobs/ssnet-tuftscluster-scripts && source run_job.sh /cluster/home/twongj01/grid_jobs/ssnet-tuftscluster-scripts /cluster/home/twongj01/grid_jobs/ssnet-tuftscluster-scripts/inputlists /cluster/kappa/90-days-archive/wongjiradlab/larbys/data/mcc8.1/numu_1muNpfiltered/out_week0626/ssnet /cluster/home/twongj01/grid_jobs/ssnet-tuftscluster-scripts/rerunlist.txt"
+srun singularity exec ${CONTAINER} bash -c "cd ${WORKDIR} && source run_job.sh ${WORKDIR} ${INPUTLISTDIR} ${OUTDIR} ${JOBIDLIST}"
+
+# example
+#srun singularity exec /cluster/kappa/90-days-archive/wongjiradlab/larbys/images/singularity-dllee-ubuntu/singularity-dllee-ssnet.img bash -c "cd /cluster/home/twongj01/grid_jobs/ssnet-tuftscluster-scripts && source run_job.sh /cluster/home/twongj01/grid_jobs/ssnet-tuftscluster-scripts /cluster/home/twongj01/grid_jobs/ssnet-tuftscluster-scripts/inputlists /cluster/kappa/90-days-archive/wongjiradlab/larbys/data/mcc8.1/numu_1muNpfiltered/out_week0626/ssnet /cluster/home/twongj01/grid_jobs/ssnet-tuftscluster-scripts/rerunlist.txt"
+
